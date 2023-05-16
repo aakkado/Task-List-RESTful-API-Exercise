6849afd34e42d3f535485ace4ee8405ccbfe850c
aakkado <131195827+aakkado@users.noreply.github.com>
2023-05-16 23:58:11 +0000

    now user can add new items to their task list

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect
 from replit import db
 
 app = Flask(__name__)
@@ -8,9 +8,16 @@ def home():
   items = db.keys()
   return render_template('home.html', items=items)
 
+@app.route("/add-task", methods=['POST'])
+def add_task():
+  task = request.form.get("task")
+  description = request.form.get("description")
+  db[task] = description
+  return redirect("/")
+
 @app.route("/new-task")
 def new_task():
     return render_template('new.html')
-
+  
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
